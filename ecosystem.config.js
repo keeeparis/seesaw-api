5fae5e907fc178f1933991dae90e885b957ff3f2
Vladimir Trotsenko <vladimir.tr@musan.kz>
2024-01-17 12:15:53 +0600

    add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'seesaw-api',
+      cwd: '/var/www/seesaw-api', // must have absolute path
+      script: 'npm',
+      args: 'start',
+      watch: true,
+      ignore_watch: ['node_modules'],
+      restart_delay: 10_000,
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
